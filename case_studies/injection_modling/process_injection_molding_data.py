e030001b0d5b316923058bf4904467be4a0f3347
Nikolai West <nikolai.west@tu-dortmund.de>
2024-09-24 12:01:55 +0200

    Updated case_studies with data

---
+++ case_studies/injection_modling/process_injection_molding_data.py
@@ -0,0 +1,4 @@
+# Task: Display the following values for all 31 cycles in the H5 file:
+# 'T801I_Value', 'T802I_Value', 'T803I_Value', 'T804I_Value', 'T805I_Value'
+
+file_path = "data/20230627.h5"
---
+++ case_studies/screw_driving/process_screw_data.py
@@ -0,0 +1,4 @@
+# Task: Display the 100 screw runs using torque and angle values
+# find these keys... tightening steps -> graph -> torque/angle values
+
+path_to_screw_data = "data/"
